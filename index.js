37dc04d7f010c4c4f0890cd191d500edf1075eef
MingmaTenzing <pasangsherpa689@gmail.com>
2022-10-19 21:02:25 +0800

    update

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-/* template_qr9voxf */
-/* service_spab4la */
-
-//  User-ID :  5lDQ2fsinXAaz-OgN //
-
-
+// user id - 5lDQ2fsinXAaz-OgN //
+// service id - service_spab4la //
+// tempalte  id - template_687c8jp //
 
 function contact(event) {
-    event.preventDefault();
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += ' modal__overlay--visible'
+  emailjs
+    .sendForm(
+      "service_spab4la",
+      "template_qr9voxf",
+      event.target,
+      "5lDQ2fsinXAaz-OgN"
+    )
+    .then(() => {
+        
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+        
+    }).catch (()=>{
+        loading.classList.remove('modal__overlay--visible')
+        alert(
+            'the email service  is not working at the moment pls contact me directly '
+        );
+    })
 
-const loading = document.querySelector(' .modal__overlay--loading')
-const success = document.querySelector( ' .modal__overlay--success')
-loading.classList += ' modal__overlay--visible'
-    setTimeout(()=> {
-        loading.classList.remove('modal__overlay--visible');
-        success.classList += ' modal__overlay--visible';
-        console.log('it worked 1')
-    },1000);
+ 
 }
