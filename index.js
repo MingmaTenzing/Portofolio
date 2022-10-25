1140e7ccee216dfe0b12f2410fc4ae488b50b6da
MingmaTenzing <pasangsherpa689@gmail.com>
2022-10-25 19:59:41 +0800

    included shapes moment

--- index.js
+++ index.js
@@ -63,3 +63,18 @@ function enabledarkmode(){
 
 
 }
+
+const scaleFactor = 1 / 20;
+
+function mouseBackground(event) {
+const shapes = document.querySelectorAll('.shape')
+  const x = event.clientX *scaleFactor;
+  const y= event.clientY  * scaleFactor;
+  
+  for (let i=0; i<shapes.length; i++) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * isOdd}px)`
+    console.log('it works')
+  }
+}
